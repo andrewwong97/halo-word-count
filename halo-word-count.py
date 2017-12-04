0d3de65c3f077957821304e35c6f0f4c1bf2482e
Andrew Wong <aznhippos@gmail.com>
2017-12-03 20:54:46 -0500

    Finish challenge

--- halo-word-count.py
+++ halo-word-count.py
@@ -1,20 +1,42 @@
 #!/usr/bin/env python
 
-import re
-import collections
+import string
 
 expected = ['the', 'and', 'to', 'of', 'i', 'you', 'a', 'my', 'hamlet', 'in']
 
+
+def clean_data(doc):
+    data = [i.strip().lower() for i in doc.split()]
+    for i in range(len(data)):
+        data[i] = strip_punctuation(data[i])
+    return data
+
+
+def strip_punctuation(word):
+    for ch in string.punctuation:
+        word = word.replace(ch, '')
+    return word
+
+
 def count_words():
-    # TODO Please implement code here to analyze the hamlet.txt file and 
-    # return an array the 10 most frequently found words in descending order of frequency.
-    # Strip punctuation and make your comparisons case-insensitive.
-    raise Exception('TODO')
+    wc = {}
+    document = open('hamlet.txt', 'r').read()
+    data = clean_data(document)
+
+    for word in data:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1
+    top_k = sorted(wc.items(), key=lambda x: x[1], reverse=True)
+
+    return [i[0] for i in top_k[:10]]
+
 
 if __name__ == '__main__':
     print('Most Frequent Words...')
     answer = count_words()
 
-    print('Answer: %s' % answer)    
+    print('Answer: %s' % answer)
     assert(answer == expected)
     print('SUCCESS!')
